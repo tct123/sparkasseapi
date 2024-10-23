a6f559085e54772a79394220f085b80ab4b56980
tct123 <42028373+tct123@users.noreply.github.com>
2024-10-23 12:17:00 +0200

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,23 @@
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
+#
+# with sync_playwright() as p:
+#    for browser_type in [p.chromium, p.firefox, p.webkit]:
+#        browser = browser_type.launch()
+#        page = browser.new_page()
+#        page.goto("http://playwright.dev")
+#        page.screenshot(path=f"example-{browser_type.name}.png")
+#        browser.close()
+#
+import logging
+from datetime import date
+import getpass
+from fints.client import FinTS3PinTanClient
 
-with sync_playwright() as p:
-    for browser_type in [p.chromium, p.firefox, p.webkit]:
-        browser = browser_type.launch()
-        page = browser.new_page()
-        page.goto("http://playwright.dev")
-        page.screenshot(path=f"example-{browser_type.name}.png")
-        browser.close()
+logging.basicConfig(level=logging.DEBUG)
+f = FinTS3PinTanClient(
+    "123456789",  # Your bank's BLZ
+    "myusername",  # Your login name
+    getpass.getpass("PIN:"),  # Your banking PIN
+    "https://hbci-pintan.gad.de/cgi-bin/hbciservlet",
+    product_id="Your product ID",  # see above
+)
